60330f24eaf8c9e10f2b619b591bb53efde7dfa6
thoriqnoer <thoriq.noer@gmail.com>
2022-02-22 14:48:52 +0700

    Import icon and images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,21 @@
 import registerBg from './images/registerBg.jpg';
 import loginBg from './images/loginBg.jpg';
+import Logo from './images/logo.svg';
+import logoCoding from './images/logo-coding.png';
+import facebook from './icon/facebook.svg';
+import github from './icon/github.svg';
+import instagram from './icon/instagram.svg';
+import twitter from './icon/twitter.svg';
+import youtube from './icon/youtube.svg';
 
-export { registerBg, loginBg };
+export {
+  registerBg,
+  loginBg,
+  Logo,
+  logoCoding,
+  facebook,
+  github,
+  instagram,
+  twitter,
+  youtube,
+};
