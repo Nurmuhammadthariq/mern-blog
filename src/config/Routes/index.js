472215b7f1f5a3bc75aaf03d24448f1cfcef7a7c
thoriqnoer <thoriq.noer@gmail.com>
2022-02-22 10:30:48 +0700

    create routing

--- src/config/Routes/index.js
+++ src/config/Routes/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Home, Login, Register } from '../../pages';
+import { Home, Login, Register, MainApp } from '../../pages';
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
         <Route path="/login">
           <Login />
         </Route>
         <Route path="/register">
           <Register />
+        </Route>
+        <Route path="/">
+          <MainApp />
         </Route>
       </Switch>
     </Router>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>This is Home page</div>;
+  return <div>Content Home page</div>;
 };
 
 export default Home;
