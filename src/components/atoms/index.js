9206bf33cbdc4906daa07661e6c13e254bca473a
thoriqnoer <thoriq.noer@gmail.com>
2022-02-22 10:09:39 +0700

    create button link

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,6 +1,6 @@
 import Input from './Input/';
 import Button from './Button/';
 import Gap from './Gap';
-import Link from './Link';
+import ButtonLink from './ButtonLink';
 
-export { Input, Button, Gap, Link };
+export { Input, Button, Gap, ButtonLink };
